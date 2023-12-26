bdca5ce01af0328585d0ae838500443a02226b15
mtelias2 <moutazelias@hotmail.com>
2023-12-26 15:35:09 +0400

    update dataprocessing.ipynb and main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+# this function will be executed when you type `python main.py`
+import time
+import sys
+import os
+from src.preprocessing.Preprocessing import folder_to_json
+import pathlib as Path
+
+
+def main():
+    """
+    This class is the entry point of the program.
+    It will run the preprocessing code from src/preprocessing
+    """
+
+
+        
+def 
+
+
+if __name__ == "__main__":
+    main().main
